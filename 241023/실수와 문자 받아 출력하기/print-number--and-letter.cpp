f98b0ca15d7a058bdf58beaf6c8833bae09d0eee
Younghoon Kim <96504568+kimhoyul@users.noreply.github.com>
2024-10-23 15:18:38 +0900

    Title: 실수와 문자 받아 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241023/실수와 문자 받아 출력하기/print-number--and-letter.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    double a, b;
+    char c;
+    cin >> c >> a >> b;
+    printf("%c\n%.2f\n%.2f", c, a, b);
+    return 0;
+}
