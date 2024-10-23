c0974f874c1dd7284fb6415a80ac4d02439bed7e
Younghoon Kim <96504568+kimhoyul@users.noreply.github.com>
2024-10-23 16:35:21 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241023/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a;
+    cin.get();
+    cin >> b;
+    cin.get();
+    cin >> c;
+    printf("%.3d-%d-%d",a, c, b);
+    return 0;
+}
